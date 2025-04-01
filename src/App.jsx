8c6be0061ac33eaa5fd23aab499b3a7d14073b49
Herman Lundby-Holen <hermanluho@gmail.com>
2025-04-01 18:59:10 +0200

    Fixed issue where content wasn't always taking up full view height.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router";
 import { MainSection } from "./MainSection.jsx";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
+import "./css/global-styles.css";
 
 /**
  * Component representing the whole application.
@@ -11,9 +12,11 @@ import { Footer } from "./components/Footer.jsx";
 export function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <MainSection />
-      <Footer />
+      <div className="app-container">
+        <Header />
+        <MainSection />
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
